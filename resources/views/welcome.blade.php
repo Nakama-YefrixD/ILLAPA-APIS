ffe7eec182739630327a6dfff0994daec0416239
YefrixD <gerson.vilca@tecsup.edu.pe>
2019-08-29 10:07:17 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ILLAPA
+                    ILLAPAS
                 </div>
 
                 <!-- <div class="links">
